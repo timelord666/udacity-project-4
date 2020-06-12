a4b2b25ee8b7448a1a45d3dcb0eaa08207251e01
timelord <timemonster666@gmail.com>
2020-06-12 19:54:27 +0500

    setup

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,15 @@
 var path = require('path')
 const express = require('express')
+const dotenv = require('dotenv');
 const mockAPIResponse = require('./mockAPI.js')
-
+const aylien = require('aylien_textapi');
+dotenv.config();
 const app = express()
+
+const nlp = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+})
 
 app.use(express.static('dist'))
 
@@ -10,12 +17,12 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
